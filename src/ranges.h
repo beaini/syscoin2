fd3d1f5b589aecaa48c00245afea2e87f2a77e5c
sidhujag <sidhujag@hotmail.com>
2018-01-29 11:19:41 -0800

    compile

--- src/ranges.h
+++ src/ranges.h
@@ -46,5 +46,5 @@ unsigned int validateRangesAndGetCount(const std::vector<CRange> &arr);
 bool compareRange(const CRange &i1, const CRange &i2);
 void mergeRanges(std::vector<CRange> &arr, std::vector<CRange> &output);
 void subtractRanges(std::vector<CRange> &arr, const std::vector<CRange> &del, std::vector<CRange> &output);
-bool doesRangeContain(const vector<CRange> &parent, const vector<CRange> &child);
+bool doesRangeContain(const std::vector<CRange> &parent, const std::vector<CRange> &child);
 #endif // RANGES_H
