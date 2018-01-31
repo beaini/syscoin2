a4990f491ee4c2f4f846159b9d184d8196370634
sidhujag <sidhujag@hotmail.com>
2018-01-31 10:34:21 -0800

    range contain tests

--- src/test/syscoin_asset_tests.cpp
+++ src/test/syscoin_asset_tests.cpp
@@ -22,12 +22,14 @@ BOOST_AUTO_TEST_CASE(generate_range_merge)
 	printf("Running generate_range_merge...\n");
 	// start with {0,0} {2,3} {6,8}, add {4,5} to it and expect {0,0} {2,8}
 	CheckRangeMerge("{0,0} {2,3} {6,8}", "{4,5}", "{0,0} {2,8}");
+
 }
 BOOST_AUTO_TEST_CASE(generate_range_subtract)
 {
 	printf("Running generate_range_subtract...\n");
 	// start with {0,9}, subtract {0,0} {2,3} {6,8} from it and expect {1,1} {4,5} {9,9}
 	CheckRangeSubtract("{0,9}", "{0,0} {2,3} {6,8}", "{1,1} {4,5} {9,9}");
+
 	CheckRangeSubtract("{1,2} {3,3} {6,10}", "{0,0} {2,2} {3,3}", "{1,1} {6,10}");
 }
 BOOST_AUTO_TEST_CASE(generate_range_contain)
@@ -35,7 +37,29 @@ BOOST_AUTO_TEST_CASE(generate_range_contain)
 	printf("Running generate_range_contain...\n");
 	// does {0,9} contain {0,0}?
 	BOOST_CHECK(DoesRangeContain("{0,9}", "{0,0}"));
+	BOOST_CHECK(DoesRangeContain("{0,2}", "{1,2}"));
+	BOOST_CHECK(DoesRangeContain("{0,3}", "{2,2}"));
+	BOOST_CHECK(DoesRangeContain("{0,0} {2,3} {6,8}", "{2,2}"));
+	BOOST_CHECK(DoesRangeContain("{0,0} {2,3} {6,8}", "{2,3}"));
+	BOOST_CHECK(DoesRangeContain("{0,0} {2,3} {6,8}", "{6,7}"));
+	BOOST_CHECK(DoesRangeContain("{0,8}", "{0,0} {2,3} {6,8}"));
+	BOOST_CHECK(DoesRangeContain("{0,8}", "{0,1} {2,4} {6,6}"));
+
 	BOOST_CHECK(!DoesRangeContain("{1,9}", "{0,0}"));
+	BOOST_CHECK(!DoesRangeContain("{1,9}", "{1,0}"));
+	BOOST_CHECK(!DoesRangeContain("{9,1}", "{1,1}"));
+	BOOST_CHECK(!DoesRangeContain("{1,9}", "{1,10}"));
+	BOOST_CHECK(!DoesRangeContain("{1,2}", "{1,3}"));
+	BOOST_CHECK(!DoesRangeContain("{1,2}", "{0,2}"));
+	BOOST_CHECK(!DoesRangeContain("{1,2}", "{0,3}"));
+	BOOST_CHECK(!DoesRangeContain("{0,0} {2,3} {6,8}", "{1,2}"));
+	BOOST_CHECK(!DoesRangeContain("{0,0} {2,3} {6,8}", "{0,1}"));
+	BOOST_CHECK(!DoesRangeContain("{0,0} {2,3} {6,8}", "{4,4}"));
+	BOOST_CHECK(!DoesRangeContain("{0,0} {2,3} {6,8}", "{4,5}"));
+	BOOST_CHECK(!DoesRangeContain("{0,0} {2,3} {6,8}", "{0,8}"));
+	BOOST_CHECK(!DoesRangeContain("{0,8}", "{0,1} {2,4} {6,9}"));
+	BOOST_CHECK(!DoesRangeContain("{0,8}", "{0,9} {2,4} {6,8}"));
+
 
 }
 BOOST_AUTO_TEST_SUITE_END ()
