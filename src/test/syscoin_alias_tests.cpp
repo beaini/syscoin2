08f66ae53f4586f2113b89fb3819522f72db98e3
sidhujag <sidhujag@hotmail.com>
2018-02-19 15:29:33 -0800

    update tests

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -21,6 +21,7 @@ BOOST_FIXTURE_TEST_SUITE (syscoin_alias_tests, BasicSyscoinTestingSetup)
 const unsigned int MAX_ALIAS_UPDATES_PER_BLOCK = 5;
 BOOST_AUTO_TEST_CASE (generate_big_aliasdata)
 {
+	ECC_Start();
 	GenerateBlocks(200, "node1");
 	GenerateBlocks(200, "node2");
 	GenerateBlocks(200, "node3");
